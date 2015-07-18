acaabf53f62836952f1b67120112a05cdb29e06f
vorfeed_rah <vorfeed@gmail.com>
2015-07-18 13:14:41 -0600

    More changes whee

--- watchdog_sketch/watchdog_sketch.ino
+++ watchdog_sketch/watchdog_sketch.ino
@@ -3,22 +3,23 @@
 #include <Ethernet.h>
 #include <EthernetUdp.h>  
 
+#define WD_PIN 4
+
 //the Arduino's IP and MAC
 IPAddress my_ip(192, 168, 1, 222);
 byte mac[] = { 0x02, 0x00, 0x00, 0x00, 0x00, 0x17 };
 
-// Watchdog server IP and port. The Watchdog won't connect for the HW Watchdog
-// test, but we need to set this up anyway
+// Watchdog server IP and port
 IPAddress wd_server_ip(192, 168, 1, 17);
 const unsigned int wd_server_port = 6666;
 
-Watchdog watchdog(my_ip, mac, wd_server_ip, wd_server_port);
+Watchdog watchdog(my_ip, mac, wd_server_ip, wd_server_port, WD_PIN);
 
 // the setup routine runs once when you press reset:
 void setup() {
   Ethernet.begin(mac, my_ip);
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(115200);  
+  Serial.begin(115200);
   Serial.println("Arduino reset");
   
   // Send an initial heartbeat.
