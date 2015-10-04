78a6620a2c42421b30a045644fa6cf453c859b64
vorfeed_rah <vorfeed@gmail.com>
2015-10-03 20:54:58 -0600

    fixed memory leak / reset bug

--- watchdog_sketch/watchdog_sketch.ino
+++ watchdog_sketch/watchdog_sketch.ino
@@ -6,6 +6,11 @@
 #define WD_PIN A0 //for analog pin 0 (default)
 //#define WD_PIN 4 //for digital pin 4 (optional)
 
+int WD_DEBUG = 0; /* set to 1 to test the hardware watchdog.
+                     set to 0 for production. DO NOT leave this as 1 
+                     in production or output will eat memory and 
+                     Arduino may endlessly reset! */
+
 //the Arduino's IP and MAC
 IPAddress my_ip(10, 42, 16, 222);
 byte mac[] = { 0x02, 0x00, 0x00, 0x00, 0x00, 0x17 };
@@ -14,7 +19,7 @@ byte mac[] = { 0x02, 0x00, 0x00, 0x00, 0x00, 0x17 };
 IPAddress wd_server_ip(10, 42, 16, 17);
 const unsigned int wd_server_port = 6666;
 
-Watchdog watchdog(my_ip, mac, wd_server_ip, wd_server_port, WD_PIN);
+Watchdog watchdog(my_ip, mac, wd_server_ip, wd_server_port, WD_PIN, WD_DEBUG);
 
 // the setup routine runs once when you press reset:
 void setup() {
